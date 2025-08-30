fc623ce4fd2b0756c1e136ca583c1b954c540952
Sean Malloy <sfmalloy@outlook.com>
2025-08-30 09:06:42 -0400

    feat(story2): quest 2 part 1

--- stories/02-the-entertainment-hub/quests/quest02.go
+++ stories/02-the-entertainment-hub/quests/quest02.go
@@ -2,6 +2,7 @@ package quests
 
 import (
 	"os"
+	"strings"
 )
 
 type Quest02 struct{}
@@ -13,17 +14,29 @@ func (q Quest02) Id() uint {
 func (q Quest02) Parse(file *os.File, info os.FileInfo) string {
 	input := make([]byte, info.Size())
 	file.Read(input)
-	return string(input)
+	return strings.Trim(string(input), "\n")
 }
 
-func (q Quest02) Part1(input string) string {
-	return input
+func (q Quest02) Part1(input string) int {
+	i := 0
+	b := 0
+	throws := 0
+	balloons := "RGB"
+	for i < len(input) {
+		throws += 1
+		for i < len(input) && balloons[b] == input[i] {
+			i += 1
+		}
+		i += 1
+		b = (b + 1) % 3
+	}
+	return throws
 }
 
-func (q Quest02) Part2(input string) string {
-	return input
+func (q Quest02) Part2(input string) int {
+	return 0
 }
 
-func (q Quest02) Part3(input string) string {
-	return input
+func (q Quest02) Part3(input string) int {
+	return 0
 }
