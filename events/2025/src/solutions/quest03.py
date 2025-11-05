36929a917f7a6760bd6fd606327c9df28c98e00a
Sean Malloy <sfmalloy@outlook.com>
2025-11-05 18:20:52 -0500

    feat(2025): finish quest 3

---
+++ events/2025/src/solutions/quest03.py
@@ -0,0 +1,36 @@
+from io import TextIOWrapper
+from lib.quest import app
+
+
+@app.parser(quest=3)
+def parse(file: TextIOWrapper):
+    return list(map(int, file.read().strip().split(',')))
+
+
+@app.solver(quest=3, part=1)
+def part1(a: list[int]):
+    return sum(set(sorted(a, reverse=True)))
+
+
+@app.solver(quest=3, part=2)
+def part2(a: list[int]):
+    return sum(list(set(sorted(a)))[:20])
+
+
+@app.solver(quest=3, part=3)
+def part3(a: list[int]):
+    s = sorted(a, reverse=True)
+    count = 0
+    while len(s) > 0:
+        m = s[0]
+        p = []
+        i = 1
+        while i < len(s):
+            if s[i] >= m:
+                p.append(s[i])
+            else:
+                m = s[i]
+            i += 1
+        count += 1
+        s = p
+    return count
