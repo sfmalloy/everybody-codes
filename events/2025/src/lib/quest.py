f9cbea11999d4731f4f8f08750cb4b8d31e5b113
Sean Malloy <sfmalloy@outlook.com>
2025-11-05 21:58:19 -0500

    refactor(2025): split parse and solver logic to get run times

--- events/2025/src/lib/quest.py
+++ events/2025/src/lib/quest.py
@@ -1,15 +1,24 @@
 import inspect
 import os
 
-from typing import Callable, Any
+from dataclasses import dataclass
 from functools import wraps
 from io import TextIOWrapper
 from pathlib import Path
+from timeit import default_timer as timer
+from typing import Callable, Any
 
 from lib.api import download
 
 
 ParserFn = Callable[[TextIOWrapper], Any] | Callable[[TextIOWrapper, int], Any]
+
+
+@dataclass
+class Result:
+    answer: Any = None
+    parse_time: float = None
+    solve_time: float = None
 
 
 class QuestContainer:
@@ -23,43 +32,67 @@ class QuestContainer:
     def solver(self, *, quest: int, part: int):
         def decorator(fn: Callable):
             @wraps(fn)
-            def solver(filename: str | None = None):
-                input_path = (
-                    Path(filename)
-                    if filename
-                    else (Path(f'inputs/quest{quest:02d}/part{part}.txt'))
-                )
-                if not input_path.exists():
-                    if filename:
-                        raise Exception(f'Input file "{input_path}" not found')
-                    download(int(os.getenv('EC_EVENT')), quest, part)
-                with open(input_path) as file:
-                    parser = self._parsers.get(quest)
-                    if not parser:
-                        return fn(file)
-                    if len(inspect.signature(parser).parameters) == 2:
-                        parsed = parser(file, part)
-                    else:
-                        parsed = parser(file)
-                    if isinstance(parsed, tuple):
-                        return fn(*parsed)
-                    return fn(parsed)
+            def solver_wrapper(args: tuple[Any, ...]):
+                return self.__time_fn(fn, args)
 
-            self._solvers[(quest, part)] = solver
+            self._solvers[(quest, part)] = solver_wrapper
 
         return decorator
 
     def parser(self, *, quest: int):
         def decorator(fn: ParserFn):
-            self._parsers[quest] = fn
+            @wraps(fn)
+            def parser_wrapper(part: int, filename: str | None = None):
+                input_path = self.__validate_input_path(quest, part, filename)
+
+                with open(input_path) as file:
+                    if len(inspect.signature(fn).parameters) == 2:
+                        return fn(file, part)
+                    return fn(file)
+
+            self._parsers[quest] = parser_wrapper
 
         return decorator
 
-    def run(self, quest: int, part: int, filename: str | None = None):
+    def run(self, quest: int, part: int, filename: str | None = None) -> Result:
+        parser = self._parsers.get(quest, None)
+        if parser:
+            ipt, parse_time = self.__time_fn(parser, (part, filename))
+        else:
+            ipt, parse_time = self.__time_fn(
+                self.__default_parser, (quest, part, filename)
+            )
+        if not isinstance(ipt, tuple):
+            ipt = (ipt,)
+
         solver = self._solvers.get((quest, part), None)
         if not solver:
-            raise Exception('Quest not found')
-        return solver(filename)
+            raise Exception('Quest solver not found')
+        answer, solve_time = solver(ipt)
+        return Result(answer, parse_time, solve_time)
+
+    def __time_fn(self, fn: Callable, ipt: tuple[Any, ...]) -> tuple[Any, float]:
+        start = timer()
+        answer = fn(*ipt)
+        end = timer()
+        return answer, (end - start)
+
+    def __default_parser(self, quest: int, part: int, filename: str | None):
+        input_path = self.__validate_input_path(quest, part, filename)
+        with open(input_path) as file:
+            return file
+
+    def __validate_input_path(self, quest: int, part: int, filename: str | None):
+        input_path = (
+            Path(filename)
+            if filename
+            else (Path(f'inputs/quest{quest:02d}/part{part}.txt'))
+        )
+        if not input_path.exists():
+            if filename:
+                raise Exception(f'Input file "{input_path}" not found')
+            download(int(os.getenv('EC_EVENT')), quest, part)
+        return input_path
 
 
 app = QuestContainer()
--- events/2025/src/main.py
+++ events/2025/src/main.py
@@ -14,13 +14,15 @@ def main():
     args = load_arguments()
 
     if args.use_test_input:
-        answer = app.run(args.quest, args.part, 'test.txt')
+        result = app.run(args.quest, args.part, 'test.txt')
     elif args.file:
-        answer = app.run(args.quest, args.part, args.file)
+        result = app.run(args.quest, args.part, args.file)
     else:
-        answer = app.run(args.quest, args.part)
-    pyperclip.copy(answer)
-    print(answer)
+        result = app.run(args.quest, args.part)
+    pyperclip.copy(result.answer)
+    print(
+        f'Answer:     {result.answer}\n\nParse time: {result.parse_time * 1000:.03f}ms\nSolve time: {result.solve_time * 1000:.03f}ms\nTotal Time: {(result.parse_time + result.solve_time) * 1000:.03f}ms'
+    )
 
 
 if __name__ == '__main__':
